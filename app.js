c8aebf79fa8ab1a2ba5fab287f7d1b9289063600
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-04-13 14:47:28 +0100

    added index and about pages

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require ('path');
+const exphbs = require('express-handlebars');
 
 
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('handlebars', exphbs({defaultLayout: 'home'}) );
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res)=>{
 
-    res.send("It's working");
-})
+    res.render('home/index');
+});
+
+app.get('/about', (req, res)=>{
+
+    res.render('about/index');
+});
 
 app.listen(3000, ()=>{
 
